31896d2cffc29b522ad8fc361be25c17ed5f9707
Juan Miguel Villegas <70586029+unovil@users.noreply.github.com>
2024-03-14 14:44:34 +0800

    added lucia login auth fix

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -7,4 +7,21 @@ export const load: PageServerLoad = async (event) => {
 	if (!event.locals.user) {
 		redirect(302, "/login");
 	}
+
+	return {
+		username: event.locals.user.firstName + " " + event.locals.user.lastName
+	}
 };
+
+export const actions = {
+    default: async (event) => {
+        const sessionId = event.cookies.get("auth_session") ?? "";
+        await lucia.invalidateSession(sessionId);
+
+        event.cookies.delete("auth_session", { path: "/" });
+        event.locals.user = null;
+        event.locals.session = null;
+
+        redirect(302, "/");
+    }
+} satisfies Actions;
