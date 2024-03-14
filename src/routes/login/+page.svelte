229c47d010d7b637a54b2a48fc632f6eb8517157
Hayah Reyes <sheiryhayah@gmail.com>
2024-03-14 10:06:15 +0800

    added login design

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -16,16 +16,26 @@
   <p>{form.error}</p>
 {/if}
 
-<form method="post" use:enhance>
-  <input
-    type="email"
-    placeholder="Email address"
-    name="email"
-  />
-  <br />
-  <input type="password" placeholder="Password" name="password" />
-  <br />
-  <button type="submit">Login</button>
-</form>
-
-<a href="/register" class="font-bold">Not a user yet? Register here.</a>
+<div class="flex h-screen items-center justify-center bg-gray-100">
+  <div class="bg-white rounded-lg p-6 shadow-md text-center font-alatsi">
+    <h2 class="text-lg font-semi-bold mb-6 flex items-center justify-center font-trocchi text-log-in-green transform scale-150">
+      <img src="/logo.png" alt="Logo" class="h-8 mr-2">
+      Roamer
+    </h2>
+    
+    <form method="post">
+      <input
+        type="email"
+        placeholder="Email address"
+        name="email"
+        class="block border border-gray-300 rounded-md w-full p-2 mb-2 shadow"
+      />
+      <div class="relative">
+        <input type="password" placeholder="Password" name="password" class="block border border-gray-300 rounded-md w-full p-2 mb-2 shadow" id="password" />
+        <button type="button" on:click={()=>{}} class="absolute inset-y-0 right-0 px-3 py-2 bg-gray-200 text-gray-600 rounded-md">Show</button>
+      </div>
+      <button type="submit" class="bg-log-in-green text-white rounded-md px-4 py-2 shadow hover:bg-green-500 transition duration-300 ease-in-out mt-5">Login</button>
+    </form>
+    <div class="mt-5">Not a user yet? <a href="/register" class="text-green-500 mt-1 hover:text-log-in-green">Register here.</a></div>
+  </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'log-in-green': '#478665',
+      },
+    },
+    fontFace: {
+      'trocchi': ['Trocchi', 'sans-serif']
+    }
   },
   plugins: [],
 }
-
