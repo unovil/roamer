1b6f271e21b7126b30eb9bef8a84bb24aaa2d787
Juan Miguel Villegas <70586029+unovil@users.noreply.github.com>
2024-04-18 07:56:10 +0800

    added error.svelte styling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,23 +1,27 @@
 <script lang="ts">
     import { page } from "$app/stores";
+
+    let statusMessage: string = "";
+
+    switch ($page.status) {
+        case 404:
+            statusMessage = "Sorry, the page you requested does not exist.";
+            break;
+        case 400:
+            statusMessage = "Sorry, you may have made a bad request. Retype the URL or go back to the homepage.";
+            break;
+        case 401:
+            statusMessage = "Sorry, you are not authorized to view this page.";
+            break;
+        default:
+            statusMessage = $page.error?.message || "";
+            break;
+    }
 </script>
 
-<!-- request error -->
-{#if $page.status === 404}
-<h1>404</h1>
-<p>Sorry, the page you requested does not exist.</p>
+<div class="flex flex-col items-center justify-center min-h-screen text-center bg-red-100">
+<h1 class ="text-9xl text-red-800 font-bold mb-2">{$page.status}</h1>
+<p class = "text-xl text-red-800 font-semibold mb-10">{statusMessage}</p>
 
-{:else if $page.status === 400}
-<h1>400</h1>
-<p>Sorry, you may have made a bad request. Retype the URL or go back to the homepage.</p>
-
-<!-- unauthorized error -->
-{:else if $page.status === 401}
-<h1>401</h1>
-<p>Sorry, you are not authorized to view this page.</p>
-
-<!-- general error -->
-{:else}
-<h1>{$page.status}</h1>
-<p>{$page?.error?.message}</p>
-{/if}
+<img src="/uploads/38b33416-528b-4eac-aadb-6c19cd9e143e.jpg" class="w-20 h-20 fixed bottom-0" alt=""/>
+</div>
