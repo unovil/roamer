2b9d6975b048f38b9a96f37c2f4b42ef6ed71b8d
Jairah Castrence <castrencejairahmae@gmail.com>
2024-04-28 00:15:03 +0800

    initial facility id page, fixed blocked dates and description box

--- src/routes/facility/[facilityId]/+page.svelte
+++ src/routes/facility/[facilityId]/+page.svelte
@@ -12,46 +12,55 @@
     : [];
 </script>
 
-<a href={`${$page.url}/booking`}>Roam</a>
-<br />
+<div
+  class="grid h-screen grid-cols-3 grid-rows-8 bg-white text-center"
+  style="grid-template-columns: 15% 70% 15%;"
+>
+  <div class="col-start-2 col-end-3 row-start-2 row-end-3 flex flex-col  items-center">
+    <div class="flex items-start">
+      <div
+  class="grid grid-cols-2 grid-rows-1 bg-white text-center"
+  style="grid-template-columns: 20% 20% 60%;"
+>
+      <img class="w-20 h-20 ml-0" src={`/${data.facility.image}`} alt="" />
 
-{#if data.isBookSuccess}
-  <p>Booking success!</p>
-{/if}
+      <div class ="mx-1">
+        <h1>{data.facility.name}</h1>
+        
+        <p class = "font-semibold">Admin: {#each admins as admin (admin.id)}
+          <p>
+            {admin.user.firstName + " " + admin.user.lastName}
+            <i>({admin.user.email})</i>
+          </p>
+        {/each}</p>
+        
+        <p class = "font-semibold">Department: {data.facility.department}</p>
 
-<br />
+        
+      </div>
+      <a href={`${$page.url}/booking`} class="ml-auto mt-5 bg-log-in-green  text-white shadow transition duration-300 ease-in-out hover:bg-green-500 font-bold py-2 px-4 rounded">Roam</a>
+    </div>
+</div>
+    <br />
+    {#if data.isBookSuccess}
+    <p class ="ml-96">Booking success!</p>
+  {/if}
+    <div class=" shadow p-4 border border-gray-300 rounded grid grid-cols-2 gap-4 mt-4 w-4/5 h-full">
+      <div>
+        <h2 class = "font-bold text-xl text-log-in-green">Blocked Dates</h2>
+        {#each blockedDates as date}
+          <p>
+            {new Date(date.start).toLocaleString()} - {new Date(
+              date.end,
+            ).toLocaleString()}
+          </p>
+        {/each}
+      </div>
 
-Name
-<h1>{data.facility.name}</h1>
-<br />
-
-Blocked Dates
-{#each blockedDates as date}
-  <p>
-    {new Date(date.start).toLocaleString()} - {new Date(
-      date.end,
-    ).toLocaleString()}
-  </p>
-{/each}
-<br />
-<br />
-
-Admins
-{#each admins as admin (admin.id)}
-  <p>
-    {admin.user.firstName + " " + admin.user.lastName}
-    <i>({admin.user.email})</i>
-  </p>
-{/each}
-<br />
-
-Department
-<p>{data.facility.department}</p>
-<br />
-
-Description
-<p>{data.facility.description}</p>
-<br />
-
-Image
-<img src={`/${data.facility.image}`} alt="" />
+      <div>
+        <h2 class = "font-bold text-xl text-log-in-green">Description</h2>
+        <p>{data.facility.description}</p>
+      </div>
+    </div>
+  </div>
+</div>
